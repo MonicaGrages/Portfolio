ef6ef6c80920e731e166e0a3aa2166c08229106e
MonicaGrages <mgrages1@gmail.com>
2017-05-14 16:57:43 -0400

    worked on responsive styling

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ $('.page-header').on('mouseout', function(event) {
 
 $('.page-header').on('mouseover', function() {
   if ($('.page-header').children().length === 1) {
-    $('.page-header').append("<nav class='header-nav'><ul><li><a href='#' class='clicked'>Projects</a></li><li><a href='https://www.linkedin.com/in/monicagrages/'>LinkedIn</a></li><li><a href='https://github.com/MonicaGrages'>Github</a></li><li><a href='mailto:mgrages1@gmail.com?Subject=Web%20Development%20Inquiry'target='_top'>Email Me</a></li></ul></nav>");
+    $('.page-header').append("<ul class='header-nav'><li><a href='#' class='clicked'>Projects</a></li><li><a href='https://www.linkedin.com/in/monicagrages/'>LinkedIn</a></li><li><a href='https://github.com/MonicaGrages'>Github</a></li><li><a href='mailto:mgrages1@gmail.com?Subject=Web%20Development%20Inquiry'target='_top'>Email Me</a></li></ul>");
   };
   $('.page-header').css('justify-content', 'space-between');
 });
