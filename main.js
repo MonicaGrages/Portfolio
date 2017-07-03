088a5a5fc41df9c183beced9889e3789c5f26cdb
MonicaGrages <mgrages1@gmail.com>
2017-07-03 12:43:29 -0400

    swapped some colors for borders on hover

--- main.js
+++ main.js
@@ -1,17 +1,20 @@
 // $(document).ready (function() {
 
 
-// $('.page-header').on('mouseout', function(event) {
+$('.page-header').on('mouseout', function(event) {
 //   $('.header-nav').remove();
-//   $('.page-header').css('justify-content','center');
-// });
+  // $('.page-header').css('justify-content','center');
+});
 
-// $('.page-header').on('mouseover', function() {
+$('.page-header').on('mouseover', function() {
+
 //   if ($('.page-header').children().length === 1) {
 //     $('.page-header').append("<ul class='header-nav'><li><a href='#' class='clicked'>Projects</a></li><li><a href='https://www.linkedin.com/in/monicagrages/'>LinkedIn</a></li><li><a href='https://github.com/MonicaGrages'>Github</a></li><li><a href='mailto:mgrages1@gmail.com?Subject=Web%20Development%20Inquiry'target='_top'>Email Me</a></li></ul>");
-//   };
+  };
 //   $('.page-header').css('justify-content', 'space-between');
-// });
+
+
+});
 
 
 
