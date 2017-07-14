938d4c8fa17a3bbb5e808706ad98c1d331ae67ba
MonicaGrages <mgrages1@gmail.com>
2017-07-14 13:42:12 -0400

    added adoptr

--- main.js
+++ main.js
@@ -8,6 +8,12 @@ $(document).ready (function() {
     $('.header-nav-link').addClass("nav-link-blue");
   });
 
+  $('.image-container').on('mouseover', function(event) {
+    // $('.header-nav-link').addClass("nav-link-blue");
+//make the other ones small and the target big
+      console.log(event.target);
+
+    });
 });
 
 
